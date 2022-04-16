ecd6cc9ac0ca2f745a629bb02ead90353d9c2617
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-04-16 23:02:46 +0200

    Add sms manager test feature

---
+++ tests/Feature/SmsManagerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Shabayek\Sms\Tests\Feature;
+
+use Shabayek\Sms\Facade\Sms;
+use InvalidArgumentException;
+use Shabayek\Sms\Drivers\SmsEg;
+use Shabayek\Sms\Tests\TestCase;
+use Shabayek\Sms\Drivers\SmsMisr;
+use Shabayek\Sms\Drivers\SmsNull;
+
+/**
+ * SmsManagerTest class.
+ * @author Esmail Shabayek <esmail.shabayek@gmail.com>
+ */
+class SmsManager extends TestCase
+{
+    /** @test */
+    public function it_can_sms_manager_instance_of_smseg()
+    {
+        $this->setConfig('smseg');
+
+        $sms = Sms::driver();
+
+        $this->assertInstanceOf(SmsEg::class, $sms);
+    }
+    /** @test */
+    public function it_can_sms_manager_instance_of_smsmisr()
+    {
+        $this->setConfig('smsmisr');
+
+        $sms = Sms::driver();
+
+        $this->assertInstanceOf(SmsMisr::class, $sms);
+    }
+    /** @test */
+    public function it_can_sms_manager_instance_of_sms_null()
+    {
+        $sms = Sms::driver();
+
+        $this->assertInstanceOf(SmsNull::class, $sms);
+    }
+    /** @test */
+    public function it_can_sms_manager_throw_exception_with_undefined_driver()
+    {
+        $this->setConfig('test');
+
+        $this->expectException(InvalidArgumentException::class);
+
+        Sms::driver();
+    }
+    /**
+     * Set config.
+     *
+     * @param string $config
+     * @return void
+     */
+    private function setConfig(string $config)
+    {
+        config()->set('sms.default', $config);
+    }
+}
